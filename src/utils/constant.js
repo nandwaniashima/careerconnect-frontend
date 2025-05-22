9e06cd1aa19e088bf427bea82ecfaf24b1159cfd
nandwaniashima <ashimanandwani5@gmail.com>
2025-05-23 00:26:32 +0530

    adding frontend on git

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,4 @@
-cor
+export const USER_API_END_POINT="https://careerconnect-backend-5xq5.vercel.app/api/v1/user";
+export const JOB_API_END_POINT="https://careerconnect-backend-5xq5.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT="https://careerconnect-backend-5xq5.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT="https://careerconnect-backend-5xq5.vercel.app/api/v1/company";
